0978023c82f2b77921319e8a1707ec9d297db2b6
DONGHWAN, KIM <kdhpopoya@gmail.com>
2020-05-20 15:49:18 +0900

    fix: add validate jwt

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,24 +2,48 @@ import Vue from 'vue'
 import VueRouter, {RouteConfig} from 'vue-router'
 import Login from "@/views/Login.vue";
 import Search from "@/views/Search.vue";
+import accountService, {ApiPath, TokenValidationRequest} from "@/service/accountService";
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Search',
-    component: Search
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
+    {
+        path: '*',
+        redirect: '/',
+    },
+    {
+        path: '/',
+        name: 'Search',
+        component: Search,
+        beforeEnter: (to, from, next) => {
+            const jwt = window.localStorage.getItem('jwt');
+            if (!jwt) {
+                console.log('JWT NOT FOUND');
+                next({path: '/signin'});
+                return;
+            }
+
+            const token: TokenValidationRequest = {
+                jwt: jwt
+            }
+
+            accountService().restfulPost(ApiPath.VALIDATE, token).then(() => {
+                console.log("AVAILABLE TOKEN");
+                next();
+            }).catch((error) => {
+                console.error(error);
+            });
+        }
+    },
+    {
+        path: '/signin',
+        name: 'Login',
+        component: Login
+    },
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
