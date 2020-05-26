907f50f034cdefe92cdbbdfb94aa994dfe73ae61
DONGHWAN, KIM <kdhpopoya@gmail.com>
2020-05-26 14:30:35 +0900

    feat: add searchService

---
+++ src/service/searchService.ts
@@ -0,0 +1,21 @@
+import BaseService from "@/service/baseService";
+
+let INSTANCE: SearchService;
+
+export enum ApiPath {
+    SEARCH = 'naming',
+    AUTOCOMPLETE = 'naming/autocomplete'
+}
+
+export class SearchService extends BaseService {
+    constructor() {
+        super();
+    }
+}
+
+export default function searchService(): SearchService {
+    if (!INSTANCE) {
+        INSTANCE = new SearchService();
+    }
+    return INSTANCE;
+}
