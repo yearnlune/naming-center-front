162dc60651c73bcf5326c018652e9b27283946fb
DONGHWAN, KIM <kdhpopoya@gmail.com>
2020-05-29 18:01:39 +0900

    Add search & autoComplete

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,17 +1,80 @@
 <template>
-    <div class="d-flex flex-column align-center justify-center ">
-        <div class="align-start"> test </div>
+    <div class="search">
+        <v-container fluid class="search-container">
+            <v-row class="col-6">
+                <v-col>
+                    <v-form>
+                        <v-autocomplete
+                                id="search-input"
+                                v-model="search"
+                                prepend-inner-icon="mdi-magnify"
+                                :items="autocompleteItems"
+                                :search-input.sync="searchItem"
+                                :loading="loading"
+                                outlined
+                                rounded
+                                clearable
+                                hide-selected
+                                autofocus
+                        ></v-autocomplete>
+                    </v-form>
+                </v-col>
+            </v-row>
+        </v-container>
     </div>
 </template>
 
 <script lang="ts">
-    import {Component, Vue} from "vue-property-decorator";
+    import {Component, Vue, Watch} from "vue-property-decorator";
+    import searchService, {ApiPath, SearchService} from "@/service/searchService";
 
     @Component({
         name: "Search",
     })
 
     export default class Search extends Vue {
+        private search = '';
+        private searchItem = '';
+        private autocompleteItems = [];
+        private loading = false;
+        private searchService: SearchService = searchService();
+        private timer: NodeJS.Timeout | number = 0;
+        private TYPING_PENDING_TIMEOUT = 200;
 
+        @Watch("searchItem")
+        onWatchSearchItem(neo: string) {
+            let item = neo;
+            if (!item) {
+                item = '';
+            }
+            this.getAutocomplete(item);
+        }
+
+        private getAutocomplete(naming: string) {
+            if (typeof this.timer === 'number') {
+                clearTimeout(this.timer);
+            }
+
+            this.timer = setTimeout(() => {
+                this.loading = true;
+                this.searchService.restfulGet(ApiPath.AUTOCOMPLETE, naming)
+                    .then((response) => {
+                        this.autocompleteItems = response.data?.namingList || [];
+                    }).catch((error) => {
+                    console.error(error);
+                }).finally(() => {
+                    this.loading = false;
+                })
+            }, this.TYPING_PENDING_TIMEOUT);
+        }
     }
 </script>
+
+<style lang="scss">
+    .search {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+</style>
