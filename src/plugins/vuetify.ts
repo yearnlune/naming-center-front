1def8628b90fa47a9216d3c0218a0280351289dd
DongHwan Kim <kdhpopoya@gmail.com>
2020-06-18 15:44:02 +0900

    feat: update vuetify

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,4 +4,20 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    icons: {
+        iconfont: "mdi",
+    },
+    theme: {
+        themes: {
+            light: {
+                primary: "#2F4475",
+                secondary: "#FFD141",
+                accent: "#FF5C08",
+                error: "#F52200",
+                info: "#17A9E1",
+                success: "#A9CC66",
+                warning: "#F2D22E"
+            }
+        }
+    }
 });
