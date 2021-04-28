c3b02c1cfab503b1ef4fb2e0c54fc6bf65ff0df7
DONGHWAN, KIM <kdhpopoya@gmail.com>
2021-04-28 19:09:02 +0900

    fix: Refactor

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,29 +1,26 @@
 <template>
-    <div class="container">
-        <SignInForm></SignInForm>
-    </div>
+  <div class="container">
+    <SignInForm></SignInForm>
+  </div>
 </template>
 
-
 <script lang="ts">
-    import SignInForm from "@/components/account/SignInForm.vue";
-    import {Component, Vue} from "vue-property-decorator";
+import SignInForm from "@/components/account/SignInForm.vue";
+import { Component, Vue } from "vue-property-decorator";
 
-    @Component({
-        name: "Login",
-        components: {
-            SignInForm
-        }
-    })
-
-    export default class Login extends Vue {
-    }
+@Component({
+  name: "Login",
+  components: {
+    SignInForm,
+  },
+})
+export default class Login extends Vue {}
 </script>
 
 <style lang="scss">
-    .container {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 </style>
